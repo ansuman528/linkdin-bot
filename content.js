7370eacf078cc00f908e5037de4c7ed55cff8f43
Ansuman Nayak <60027681+ansuman528@users.noreply.github.com>
2022-10-30 01:20:17 +0530

    bug fix and improvement

--- content.js
+++ content.js
@@ -9,10 +9,11 @@ const readLocalStorage = async (key) => {
       });
     });
 };
-function makeLike(likeCount,content)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function makeLike(likeCount,content)
 {
-    if(content)
-    {
         for(let i=0;i<likeCount;i++)
         {
             const container = content[i].querySelector("div.feed-shared-social-actions.feed-shared-social-action-bar.social-detail-base-social-actions.feed-shared-social-action-bar--full-width.feed-shared-social-action-bar--has-social-counts > span.reactions-react-button.feed-shared-social-action-bar__action-button");
@@ -21,12 +22,14 @@ function makeLike(likeCount,content)
                 const temp = container.getElementsByTagName("button");
                 temp[0].click();
             }   
+            window.scrollBy(0,1500);
+            await sleep(1000);
         }
-    }
 }
+
 function clickFunction()
 {
-    console.log("i am here");
+    console.log("i am working");
     const submitButton = document.querySelector("div.comments-comment-box__form-container.flex-grow-1 > form > div:nth-child(2)")
     if(submitButton)
     {
@@ -35,19 +38,22 @@ function clickFunction()
         btn[0].click();
     }
 }
-function makeComment(commentCount,content)
+async function makeComment(commentCount,content)
 {
-    const  message = "Commenting for better reach";
+    const  message = "Bot testing please ignore";
     if(content)
     {
-        for(let i=0;i<commentCount;i++)
+        for(let i=commentCount-1;i>=0;i--)
         {
             const editBox = content[i].querySelector("div.comments-comment-box__form-container.flex-grow-1 > form > div > div > div.comments-comment-box-comment__text-editor > div > div.editor-container > div > div > div.ql-editor.ql-blank");
             if(editBox)
             {
                 editBox.innerHTML = message;
-                setTimeout(clickFunction, 2000);
+                setTimeout(clickFunction, 3000);
             }
+            window.scrollBy(0,-1500);
+            console.log(i);
+            await sleep(2000);
         }
     }
 }
@@ -56,9 +62,11 @@ async function myScript()
     const likeCount = await readLocalStorage('likeCount');
     const commentCount = await readLocalStorage('commentCount');
     const content = document.querySelectorAll("#main > div:nth-child(3) > div > div.scaffold-finite-scroll__content:first-child > div");
+    console.log(content.length)
+    console.log(likeCount)
     if(content)
     {
-        makeLike(likeCount,content);
+        await makeLike(likeCount,content);
         makeComment(commentCount,content);
     }
 }
